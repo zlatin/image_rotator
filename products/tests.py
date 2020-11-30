26641b8b3506f1f344425d2819498d41369af819
Maxim Zalata <zalatik@gmail.com>
2020-11-30 15:56:27 +0200

    add some tests

--- products/tests.py
+++ products/tests.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 from datetime import datetime
+from django.http import response
 
 from django.urls import reverse
 from freezegun import freeze_time
@@ -19,16 +20,38 @@ class ProductsCRUDTestCase(APITestCase):
     def test_product_detail_returns_all_fields(self):
         product = Product.objects.create(name="test1", description="fat")
         r = self.client.get(reverse("product-list"))
-        product_received = json.loads(r.content)['results'][0]
+        product_received = json.loads(r.content)["results"][0]
         self.assertEqual(
             product_received,
             {
                 "uuid": str(product.uuid),
                 "name": "test1",
                 "description": "fat",
-                "created": '2020-11-13T00:00:00Z',
+                "created": "2020-11-13T00:00:00Z",
                 "updated": None,
                 "logo": None,
                 "rotate_duration": None,
             },
         )
+
+    def test_modified_filter_product_not_modified(self):
+        product = Product.objects.create(name="test1", description="fat")
+        response = self.client.get("/products/?modified=false")
+        json_response_products = json.loads(response.content)["results"]
+        self.assertEqual(len(json_response_products), 1)
+
+    def test_modified_filter_product_been_modified(self):
+        product = Product.objects.create(name="test1", description="fat")
+        product.updated = datetime.now()
+        product.save()
+        response = self.client.get("/products/?modified=true")
+        print(response)
+        json_response_products = json.loads(response.content)["results"]
+        self.assertEqual(len(json_response_products), 1)
+
+    def test_patch_updates_updated_field(self):
+        product = Product.objects.create(name="test1", description="fat")
+        r = self.client.patch(f'/products/{product.uuid}/', {"name":"redacted"})
+        product = json.loads(r.content)
+        self.assertEqual(product['name'], 'redacted')
+        self.assertIsNotNone(product['updated'])
