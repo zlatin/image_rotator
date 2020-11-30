a163c33182577a85890144d9055debf3a1386760
Maxim Zalata <zalatik@gmail.com>
2020-11-30 15:25:58 +0200

    add test for product details

--- products/tests.py
+++ products/tests.py
@@ -1,36 +1,34 @@
-from datetime import datetime
-from django.urls import reverse
-from rest_framework.status import HTTP_200_OK
+import datetime
 import json
+from datetime import datetime
+
+from django.urls import reverse
+from freezegun import freeze_time
+from rest_framework.status import HTTP_200_OK
 from rest_framework.test import APITestCase
 
 from products.models import Product
 
-import datetime
-import json
 
+@freeze_time("2020-11-13")
 class ProductsCRUDTestCase(APITestCase):
     def test_products_endpoint_returns_200(self):
         response = self.client.get(reverse("product-list"))
         self.assertEqual(response.status_code, HTTP_200_OK)
 
-    # def test_product_detail_returns_all_fields(self):
-    #     creation_time = datetime.datetime.now()
-    #     product = Product.objects.create(
-    #         name="test1", description="fat", logo="/logo/img.jpg", created=creation_time
-    #     )
-    #     r = self.client.get(reverse("product-list"))
-    #     product_received = json.loads(r.content, object_hook=date_hook)[0]
-    #     product_received.created = 
-    #     self.assertEqual(
-    #         product_received.uu,
-    #         {
-    #             "uuid": str(product.uuid),
-    #             "name": "test1",
-    #             "description": "fat",
-    #             "created": creation_time,
-    #             "updated": None,
-    #             "logo": "http://testserver/media/logo/img.jpg",
-    #             "rotate_duration": None,
-    #         },
-    #     )
+    def test_product_detail_returns_all_fields(self):
+        product = Product.objects.create(name="test1", description="fat")
+        r = self.client.get(reverse("product-list"))
+        product_received = json.loads(r.content)['results'][0]
+        self.assertEqual(
+            product_received,
+            {
+                "uuid": str(product.uuid),
+                "name": "test1",
+                "description": "fat",
+                "created": '2020-11-13T00:00:00Z',
+                "updated": None,
+                "logo": None,
+                "rotate_duration": None,
+            },
+        )
